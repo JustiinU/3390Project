3fae77c66edf158258df551c3f961752d0dea484
quinterosd <dquinteros1@csub.edu>
2023-12-17 17:40:52 -0800

    adding server withaccount login

--- App.js
+++ App.js
@@ -7,6 +7,132 @@ import RegisterScreen from './RegisterScreen.js';
 
 const Stack = createNativeStackNavigator();
 
+//  server stuff
+//---------------------------------------------------
+const hbs = require('hbs');
+const MongoClient = require('mongodb').MongoClient;
+const passport = require('passport');
+//for localhost 
+//const Strategy = require('passport-local').Strategy;
+const authUtils = require('./utils/auth');
+const session = require('express-session');
+const flash = require('connect-flash');
+// --------------------------------------------------
+
+// Add new routes
+// --------------------------------------------------
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const authRouter = require('./routes/auth');
+var app = express();
+// --------------------------------------------------
+
+// Connect to db  Note: user accounts will populated as users are added
+// --------------------------------------------------
+MongoClient.connect('mongodb://localhost', (err, client) => {
+  if (err) {
+    throw err;
+  }
+
+  const db = client.db('account-app');
+  const users = db.collection('users');
+  app.locals.users = users;
+});
+// --------------------------------------------------
+
+// Configure passport
+// --------------------------------------------------
+passport.use(new Strategy(
+  (username, password, done) => {
+    app.locals.users.findOne({ username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      if (user.password != authUtils.hashPassword(password)) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, { id });
+});
+// --------------------------------------------------
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Set partials for handlebars
+// --------------------------------------------------
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+// --------------------------------------------------
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Configure session, passport, flash
+// --------------------------------------------------
+app.use(session({
+  secret: 'session secret',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  next();
+});
+// --------------------------------------------------
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Add new routes
+// --------------------------------------------------
+app.use('/auth', authRouter);
+// --------------------------------------------------
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
 export default function App() {
   return (
     <NavigationContainer>
@@ -49,3 +175,4 @@ export default function App() {
   );
 }
 
+module.exports = app;
--- routes/APIRoutes.js
+++
@@ -1,4 +0,0 @@
-t express = require('express')
-
-const router = express.Router()
-
